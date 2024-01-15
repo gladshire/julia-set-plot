9d752ddc8c0c7401debaf6444e742cb29e84188d
gladshire <miles.girard@gmail.com>
2024-01-15 13:05:59 -0600

    Made script multithreaded

--- julia_set.py
+++ julia_set.py
@@ -1,20 +1,22 @@
-from PIL import Image, ImageDraw
+from PIL import Image
+import numpy as np
 import sys
 import cmath
-
-Image.MAX_IMAGE_PIXELS = None
+from joblib import Parallel, delayed
 
 
 # Max iterations for graph
 MAX_ITER = 100
 
+
 def mandelbrot(c):
     z = 0
     n = 0
     while abs(z) <= 2 and n < MAX_ITER:
-        z = z*z + c
+        z = z * z + c
         n += 1
     return n
+
 
 def burningship(c):
     z = 0
@@ -26,6 +28,7 @@ def burningship(c):
         n += 1
     return n
 
+
 def custom(c):
     z = 0
     n = 0
@@ -35,52 +38,82 @@ def custom(c):
     return n
 
 
+# Function for splitting image depending on number of threads
+def splitImage(numThreads, width, height):
+    rowsPerThread = height // numThreads
+    splitPoints = []
+    for i in range(numThreads):
+        startRow = i * rowsPerThread
+        if i < numThreads:
+            endRow = startRow + rowsPerThread
+        else:
+            endRow = height
+        splitPoints.append((startRow, endRow))
+    return splitPoints
+
+
+def imgThread(juliaFunction, startRow, endRow, width, height):
+    imgMatrix = np.empty((HEIGHT, WIDTH, 3), dtype=np.uint8)
+    imgSection = imgMatrix[startRow:endRow, :]
+    for y in range(startRow, endRow):
+        for x in range(width):
+            pixelCoord = complex(
+                xlimLower + (x / width) * (xlimUpper - xlimLower),
+                ylimLower + (y / height) * (ylimUpper - ylimLower),
+            )
+            imgSection[y - startRow, x, 0] = juliaFunction(pixelCoord)
+            currNumIter = imgSection[y - startRow, x, 0]
+            currHue = itersToHue(currNumIter)
+            value = 255 if currNumIter < MAX_ITER else 0
+            imgSection[y - startRow, x] = (currHue, 255, value)
+    return startRow, endRow, imgSection
+
+
+def itersToHue(pixelValue):
+    hue = int(255 * pixelValue / MAX_ITER)
+    return hue
+
+
 if __name__ == "__main__":
-    if len(sys.argv) != 8:
-        print("Usage: python3 julia_set.py xlimL xlimU ylimL ylimU WIDTH HEIGHT filename")
+    if len(sys.argv) != 9:
+        print(
+            "Usage: python3 julia_set.py xlimL xlimU ylimL ylimU WIDTH HEIGHT filename"
+        )
         exit()
+
+
+    # Obtain image specification from arguments
     xlimLower = float(sys.argv[1])
     xlimUpper = float(sys.argv[2])
     ylimLower = float(sys.argv[3])
     ylimUpper = float(sys.argv[4])
-    WIDTH     = int(sys.argv[5])
-    HEIGHT    = int(sys.argv[6])
+    WIDTH = int(sys.argv[5])
+    HEIGHT = int(sys.argv[6])
     fname = sys.argv[7]
+    numThreads = int(sys.argv[8])
+    
+    print("Array initialized")
+    
+    # Obtain image bounds for each thread
+    splitPoints = splitImage(numThreads, WIDTH, HEIGHT)
+    print(splitPoints)
+    print("Thread split indices obtained")
+    
+    # Set desired function here
+    desiredFunction = burningship
 
-    palette = []
-
-
-    im = Image.new('HSV', (WIDTH, HEIGHT), (0, 0, 0))
-    draw = ImageDraw.Draw(im)
-
+    r = Parallel(n_jobs=numThreads, verbose=0,)(
+        delayed(imgThread)(
+            desiredFunction,
+            rowTuple[0],
+            rowTuple[1],
+            WIDTH,
+            HEIGHT,
+        )
+        for rowTuple in splitPoints
+    )
+    startRow, endRow, imgSection = zip(*r)
+    imgMatrix = np.concatenate(imgSection)
     print("Working ...")
-
-    pixel_num = WIDTH * HEIGHT
-    pixel_cur = 0
-    prog = 0
-
-    for x in range(0, WIDTH):
-        for y in range(0, HEIGHT):
-            # Convert pixel coords to complex number
-            c = complex(xlimLower + (x / WIDTH) * (xlimUpper - xlimLower),
-                        ylimLower + (y / HEIGHT) * (ylimUpper - ylimLower))
-            # Compute num of iters
-            #m = mandelbrot(c)
-            #m = burningship(c)
-            m = custom(c)
-            # Color depends on number of iters
-            hue = int(255 * m / MAX_ITER)
-            saturation = 255
-            value = 255 if m < MAX_ITER else 0
-            # Plot point
-            draw.point([x, y], (hue, saturation, value))
-            pixel_cur += 1
-            if pixel_cur % 1000 == 0:
-                prog = pixel_cur / pixel_num
-                print("Finished {:.2%}".format(prog), end = "\r")
-
-    if len(sys.argv) == 1:
-        im.convert('RGB').save('output.png', 'PNG')
-    else:
-        im.convert('RGB').save(fname, 'PNG')
-
+    finalImage = Image.fromarray(imgMatrix, mode="HSV")
+    finalImage.convert("RGB").save(fname, "PNG")
